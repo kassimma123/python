bbd6374f549584e10e7084d9a1c0a1faeaa5ff9c
kassimma123 <katpiskorz@student.agh.edu.pl>
2026-05-29 13:39:01 +0200

    Auto-sync (python): 2026-05-29 13:39:01 - automatyczna aktualizacja z zajęć.

--- 4/main.py
+++ 4/main.py
@@ -1,23 +1,53 @@
 
 #podstawowe
 class Student:
-    def __init__(self, imie: str, nazwisko: str, numer_albumu: int):
+    #konstruktor
+    def __init__(self, imie: str, nazwisko: str, indeks: int):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.numer_albumu = numer_albumu
+        self.indeks = indeks
 
+
+    #metoda wypisująca dane
     def przedstaw_sie(self):
-        return f"Cześć, jestem {self.imie} {self.nazwisko}, a mój indeks to {self.numer_albumu}. "
+        return f"Cześć, jestem {self.imie} {self.nazwisko}, a mój indeks to {self.indeks}. "
 
-#stworznie dwóch obiektów klasy student
+    #metoda specjalna 
+    def __str__(self):
+        return self.przedstaw_sie()
+
+#test
 student1 = Student("Kacper","Masło",123456)
 student2 = Student("Jola", "Masełko", 654321)
 
-#wywołanie metody przedstaw_sie 
-print(student1.przedstaw_sie())
-print(student2.przedstaw_sie())
+print(student1)
+print(student2)
+
+#zad średniozaawansowane obsłuzenie 3 miejsca po przecinku w zł -- biblioteka decimal
+#getter setter, walidator 
+class KontoBankowe:
+    def __init__(self, saldo):
+        self.__saldo = round(saldo, 2) # prywatny atrybut __saldo
+    
+    def wplac(self, kwota):
+        if kwota > 0: self.saldo += kwota
+        else: print("kwota musi byc wieksza od 0")
+    
+    def wyplac(self, kwota):
+        if kwota > 0 and kwota <= self.saldo: self.saldo -= kwota
+        else: print("kwota musi byc wieksza od 0 lub nie mozna wyplacic wiecej niz na koncie")
+    
+    def pokaz_saldo(self):
+        print(f"Aktualne saldo: {round(self.__saldo, 2)} zł")
+
+    @property
+    def saldo(self):
+        return self.__saldo # bezpieczny odczyt przez property
 
 
-
-#zad średniozaawansowane obsłuzenie 3 miejsca po przecinku w zł
-#zaawaansowane
+# Test
+konto = KontoBankowe(100)
+konto.wplac(50)
+konto.wyplac(30)
+konto.pokaz_saldo() # Wypisze aktualne saldo
+print(konto.saldo)  # Odczyt salda bez nawiasów dzięki @property
